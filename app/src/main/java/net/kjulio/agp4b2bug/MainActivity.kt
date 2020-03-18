ffd077fcd590fbc7c6800df5b735da733e3efc05
Marco Romano <marcojulioromano@gmail.com>
2020-03-18 16:44:26 +0100

    New project which reproduces the bug.

---
+++ app/src/main/java/net/kjulio/agp4b2bug/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.kjulio.agp4b2bug
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
